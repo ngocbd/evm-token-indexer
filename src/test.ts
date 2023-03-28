698feb2871852cfc34ee8fcaf30acd35545c0b68
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-03-28 10:27:59 +0700

    update deployment file for k8s

--- src/test.ts
+++ src/test.ts
@@ -14,15 +14,14 @@ const main = async () => {
   const provider = new ethers.providers.JsonRpcProvider(
     FOUR_BYTES_ETH_RPC_URL,
   );
-  // const filterWorker = new FilterEventWorker(provider);
-  // const tokenType = await filterWorker.detectTokenType('0x3bf2922f4520a8BA0c2eFC3D2a1539678DaD5e9D');
-  // console.log(tokenType);
 
-  // // const res = await filterWorker.getTokenMetaData('0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7', TokenType.ERC20);
-  // // console.log(res);
   const configService = new IndexerConfigService();
+  const counterService = new CounterService();
+
   await configService.setConfigValue(TRANSACTION_SAVE_PER_MESSSAGE, '50');
   await configService.setConfigValue(TRANSFER_EVETNS_SAVE_PER_MESSSAGE, '50');
+  await counterService.initOrResetCounter();
+
 
 
 
